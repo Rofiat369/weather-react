9bfb469ee0593e91680cc5a6f1c9a1025740e6ce
Rofiat369 <63071451+Rofiat369@users.noreply.github.com>
2021-05-23 12:29:43 -0400

    Deleted information from App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import axios from "axios";
 import './App.css';
 
 function App() {
-
-  function displayWeather(response) {
-    alert(`The weather in New York is ${response.data.main.temp}°C`)
-  }
-
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=a61759b6c49305b3341bd63820265f73&units=metric`;
-  axios.get(url).then(displayWeather);
-
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        <h1>
-          Hello World!
-      </h1>
-      </header>
-    </div>
-  );
+  return <h2>Weather App</h2>;
 }
 
 export default App;
---
+++ src/Search.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+
+export default function Search() {
+    let [city, setCity] = useState("");
+    let [message, setMessage] = useState("");
+
+    function displayWeather(response) {
+        setMessage(
+            <ul>
+                <li>Temperature: {Math.round(response.data.main.temp)}°C </li>
+                <li>Description: {response.data.weather[0].description}</li>
+                <li>Humidity: {response.data.main.humidity}%</li>
+                <li>Wind: {response.data.main.wind}km/h</li>
+                <li>
+                    {" "}
+                    <img
+                        src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+                        alt="Icon"
+                    />
+                </li>
+            </ul>
+        );
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a61759b6c49305b3341bd63820265f73&units=metric`;
+        axios.get(url).then(displayWeather);
+    }
+
+    function updateCity(event) {
+        event.preventDefault();
+        setCity(event.target.value);
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={handleSubmit}>
+                <input type="search" onChange={updateCity} />
+                <input type="submit" value="Search" />
+            </form>
+            <p>{message}</p>
+        </div>
+    );
+}
